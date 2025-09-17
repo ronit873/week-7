5f47bbe6fec1aa3ad93f76e1600bc3695285c0e5
Ronit Mishra <ronitmishra873@gmail.com>
2025-09-17 23:42:33 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+class Solution:
+    
+    def fibonacciNumbers(self,n):
+        if n == 1:
+            return [0]
+        fib = [0, 1]
+        for i in range(2, n):
+            fib.append(fib[i-1] + fib[i-2])
+        
+        return fib
